263bde14443d9ad8fc82bf87068e9cbe4c410c23
TokaXD <pedro.almeidasousah@hotmail.com>
2025-10-18 18:14:15 -0300

    refactor: improve code readability by formatting async component imports in Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,15 +12,29 @@
 </template>
 
 <script setup>
-import { defineAsyncComponent } from 'vue';
+import { defineAsyncComponent } from 'vue'
 
 // Import Components Asynchronously
-const HeroSection = defineAsyncComponent(() => import('@/components/HeroSection.vue'));
-const ServicesSection = defineAsyncComponent(() => import('@/components/ServicesSection.vue'));
-const AboutSection = defineAsyncComponent(() => import('@/components/AboutSection.vue'));
-const LatestProjSection = defineAsyncComponent(() => import('@/components/LatestProjSection.vue'));
-const ExperienceAndSkills = defineAsyncComponent(() => import('@/components/ExperienceAndSkills.vue'));
-const ContactSection = defineAsyncComponent(() => import('@/components/ContactSection.vue'));
-const Footer = defineAsyncComponent(() => import('@/components/Footer.vue'));
-const BackToTop = defineAsyncComponent(() => import('@/components/BackToTop.vue'));
+const HeroSection = defineAsyncComponent(
+  () => import('@/components/HeroSection.vue')
+)
+const ServicesSection = defineAsyncComponent(
+  () => import('@/components/ServicesSection.vue')
+)
+const AboutSection = defineAsyncComponent(
+  () => import('@/components/AboutSection.vue')
+)
+const LatestProjSection = defineAsyncComponent(
+  () => import('@/components/LatestProjSection.vue')
+)
+const ExperienceAndSkills = defineAsyncComponent(
+  () => import('@/components/ExperienceAndSkills.vue')
+)
+const ContactSection = defineAsyncComponent(
+  () => import('@/components/ContactSection.vue')
+)
+const Footer = defineAsyncComponent(() => import('@/components/Footer.vue'))
+const BackToTop = defineAsyncComponent(
+  () => import('@/components/BackToTop.vue')
+)
 </script>
